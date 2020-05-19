3ab5bd301899bf7ef739ad824780d72056ee5155
danieldiaz-2502 <daniel_diazvalencia@outlook.com>
2020-05-19 11:12:28 -0500

    Pantallas

--- src/model/Logic.java
+++ src/model/Logic.java
@@ -9,13 +9,12 @@ public class Logic {
 	String pokemones[];
 	String pokemones2[];
 	String pokemon;
-	int pantalla;
-	
+	int pantalla,mapaX,mapaY;
 	//llamar la función Gif y nombrarla 
 	Gif prueba;
 	
 	//imagenes fondo y botones
-	PImage fondoInicio, fondoNada, continuarBoton, continuarBotonN,registrarseBoton,registrarseBotonN, usuarioBoton,usuarioBotonN,pokemonEleccion;
+	PImage bayleefPerfil,pikachuPerfil,girafarigPerfil,gothoritaPerfil,shayminPerfil,emolgaPerfil,megaGardevoirPerfil,bulbasaurPerfil,manectricPerfil,luxrayPerfil,inventarioIcono,fondoRosado,pokedexVolver,inventarioVolver,usuariosVolver,ordenarPor,ordenNombre,ordenFecha,ordenTipo,pokedexIcono,fondoInicio, fondoNada, continuarBoton, continuarBotonN,registrarseBoton,registrarseBotonN, usuarioBoton,usuarioBotonN,pokemonEleccion,mapa;
 	PImage selec1,selec2,selec3;
 	
 	
@@ -40,13 +39,35 @@ public class Logic {
 		selec1=app.loadImage("img/seleccionarBotonN.png");
 		selec2=app.loadImage("img/seleccionarBotonN.png");
 		selec3=app.loadImage("img/seleccionarBotonN.png");
+		mapa=app.loadImage("img/Mapa.png");
+		pokedexIcono=app.loadImage("img/pokedex.png");
+		pokedexVolver=app.loadImage("img/pokedexVolver.png");
+		inventarioVolver=app.loadImage("img/inventarioVolver.png");
+		usuariosVolver=app.loadImage("img/usuariosVolver.png");
+		ordenarPor=app.loadImage("img/ordenarPor.png");
+		ordenNombre=app.loadImage("img/ordenNombre.png");
+		ordenFecha=app.loadImage("img/ordenFecha.png");
+		ordenTipo=app.loadImage("img/ordenTipo.png");
+		fondoRosado=app.loadImage("img/Rosado.png");
+		inventarioIcono=app.loadImage("img/inventario.png");
+		bayleefPerfil=app.loadImage("img/bayleefPerfil.png");
+		emolgaPerfil=app.loadImage("img/emolgaPerfil.png");
+		pikachuPerfil=app.loadImage("img/pikachuPerfil.png");
+		bulbasaurPerfil=app.loadImage("img/bulbasaurPerfil.png");
+		girafarigPerfil=app.loadImage("img/girafarigPerfil.png");
+		gothoritaPerfil=app.loadImage("img/gothoritaPerfil.png");
+		manectricPerfil=app.loadImage("img/manectricPerfil.png");
+		megaGardevoirPerfil=app.loadImage("img/megaGardevoirPerfil.png");
+		shayminPerfil=app.loadImage("img/shayminPerfil.png");
+		luxrayPerfil=app.loadImage("img/luxrayPerfil.png");
 		prueba.loop();
+		mapaX = -200;
+		mapaY = -100;
 		for(int i = 0; i < pokemones.length ; i++) {
 			pokemones2 = pokemones[i].split(",");
 			pokemon = pokemones2[2];
 			System.out.print(pokemon);
 		}
-		
 	}
 	
 	public void pintar() {
@@ -101,7 +122,30 @@ public class Logic {
 					if( (app.mouseX>453 && app.mouseX<583)&& (app.mouseY>298 && app.mouseY<328)){
 						app.image(selec3,453,297);
 					}
-				break;	
+				break;
+				case 2:
+					app.image(mapa,mapaX,mapaY);
+					app.image(pokedexIcono,20,310,70,54);
+					app.image(inventarioIcono,20,230);
+					break;
+				case 3:
+					app.image(fondoRosado,0,0);
+					app.image(pokedexVolver,0,0);
+					app.image(ordenarPor,0,64);
+					app.image(ordenNombre,230,64);
+					app.image(ordenTipo,450,64);
+					break;
+				case 4:
+					app.image(fondoRosado,0,0);
+					app.image(usuariosVolver,0,0);
+					app.image(ordenarPor,0,64);
+					app.image(ordenNombre,230,64);
+					app.image(ordenFecha,450,64);
+					break;
+				case 5:
+					app.image(fondoRosado,0,0);
+					app.image(inventarioVolver,0,0);
+					break;
 		
 	}
 	}
@@ -125,16 +169,39 @@ public class Logic {
 						
 			//Escoge la jirafa
 			if( (app.mouseX>120 && app.mouseX<250)&& (app.mouseY>298 && app.mouseY<328)){
-		
+				pantalla = 2;
 			}
 			//Escoge la emolga
 			if( (app.mouseX>286 && app.mouseX<417)&& (app.mouseY>298 && app.mouseY<328)){
-		
+				pantalla = 2;
 			}
 			//Escoge la bayleef
 			if( (app.mouseX>453 && app.mouseX<583)&& (app.mouseY>298 && app.mouseY<328)){
-				
+				pantalla = 2;
 			}
+			break;
+		case 2:
+			if( (app.mouseX>20 && app.mouseX<20+70)&& (app.mouseY>310 && app.mouseY<310+54)){
+				pantalla = 3;
+			}
+			if( (app.mouseX>20 && app.mouseX<20+70)&& (app.mouseY>230 && app.mouseY<230+54)){
+				pantalla = 5;
+			}
+			break;
+		case 3:
+			if( (app.mouseX>0) && (app.mouseY>0 && app.mouseY<64)){
+				pantalla = 2;
+			}
+			break;
+		case 4:
+			
+			break;
+		case 5:
+			if( (app.mouseX>0) && (app.mouseY>0 && app.mouseY<64)){
+				pantalla = 2;
+			}
+			break;
+			
 		}
 		
 		
